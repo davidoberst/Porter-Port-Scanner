310bdf276d22d9ce6ac7661125aebf934c05da30
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-15 14:19:39 -0500

    Scan Multiple ports, add JSON list with 1000 common ports

--- porter.py
+++ porter.py
@@ -1,27 +1,42 @@
 import socket
 import pyfiglet
 import argparse
+import json
 import time
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
 sock.settimeout(1)
 
+#pyfiglet text
 print(pyfiglet.figlet_format("Porter", font="small"))
 time.sleep(1)
-
 #Add parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("target",help="Type the target IP adress")
-parser.add_argument("port",help="Target Port")
 args = parser.parse_args() #read user argumentss
 
-print(f"Target:{args.target}")
-print(f"Ports:{args.port} ")
+#open json 
+with open("ports.json", "r") as f:
+ data =json.load(f) 
 
-result = sock.connect_ex(((args.target) , int(args.port)))
-if result == 0:
- print("Host is up")
-else:
- print("The host may be closed or filtered.")
+openPorts=[]
+print(f"Searching for open ports in {args.target}...") 
+for x in data["common_ports"]:
+ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket per port
+ sock.settimeout(3)
+ result = sock.connect_ex(((args.target) , int(x)))
+ if result == 0:
+  openPorts.append(x) 
+ sock.close()
+ #RESULTS
+print(f"Scan ended, open ports : {len(openPorts)} ")
+for p in openPorts:
+ print(f"{p} ---> OPEN")
+ 
+ 
+   
+
+
 
 
 
